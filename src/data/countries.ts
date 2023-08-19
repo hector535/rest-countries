9574698a9e604aa64342fd001ef71657b2a5d14b
Hector Figuereo <hectorfiguereo26@gmail.com>
2023-08-19 17:29:46 -0400

    Add countries array

---
+++ src/data/countries.ts
@@ -0,0 +1,50 @@
+export const countries = [
+  {
+    name: "Germany",
+    population: 81770900,
+    region: "Europe",
+    capital: "Berlin",
+  },
+  {
+    name: "United States of America",
+    population: 323947000,
+    region: "Americas",
+    capital: "Washington, D.C",
+  },
+  {
+    name: "Brazil",
+    population: 206135898,
+    region: "Americas",
+    capital: "Brasilia",
+  },
+  {
+    name: "Iceland",
+    population: 334300,
+    region: "Europe",
+    capital: "Reykjavik",
+  },
+  {
+    name: "Afghanistan",
+    population: 27657145,
+    region: "Asia",
+    capital: "Kabul",
+  },
+  {
+    name: "Aland Islands",
+    population: 28875,
+    region: "Europe",
+    capital: "Mariehamn",
+  },
+  {
+    name: "Albania",
+    population: 2886026,
+    region: "Europe",
+    capital: "Tirana",
+  },
+  {
+    name: "Algeria",
+    population: 404000000,
+    region: "Africa",
+    capital: "Algiers",
+  },
+];
