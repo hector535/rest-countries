e6981ea10cea0be28cf7f2bdb122b2e76bde8ea0
Hector Figuereo <hectorfiguereo26@gmail.com>
2023-08-18 23:06:56 -0400

    Create file with an array of regions

---
+++ src/data/regions.ts
@@ -0,0 +1,22 @@
+export const data = [
+  {
+    value: 1,
+    name: "Africa",
+  },
+  {
+    value: 2,
+    name: "America",
+  },
+  {
+    value: 3,
+    name: "Asia",
+  },
+  {
+    value: 4,
+    name: "Europe",
+  },
+  {
+    value: 5,
+    name: "Oceania",
+  },
+];
