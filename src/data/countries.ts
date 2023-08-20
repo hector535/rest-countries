f00bdbabab9b201d905ffbf1ab6277b05e50932e
Hector Figuereo <hectorfiguereo26@gmail.com>
2023-08-20 10:46:58 -0400

    Add flag object to countries array

--- src/data/countries.ts
+++ src/data/countries.ts
@@ -4,47 +4,80 @@ export const countries = [
     population: 81770900,
     region: "Europe",
     capital: "Berlin",
+    flag: {
+      imageURL: "https://flagcdn.com/de.svg",
+      alt: "The flag of Germany is composed of three equal horizontal bands of black, red and gold.",
+    },
   },
   {
     name: "United States of America",
     population: 323947000,
     region: "Americas",
     capital: "Washington, D.C",
+    flag: {
+      imageURL: "https://flagcdn.com/us.svg",
+      alt: "The flag of the United States of America is composed of thirteen equal horizontal bands of red alternating with white. A blue rectangle, bearing fifty small five-pointed white stars arranged in nine rows where rows of six stars alternate with rows of five stars, is superimposed in the canton.",
+    },
   },
   {
     name: "Brazil",
     population: 206135898,
     region: "Americas",
     capital: "Brasilia",
+    flag: {
+      imageURL: "https://flagcdn.com/br.svg",
+      alt: "The flag of Brazil has a green field with a large yellow rhombus in the center. Within the rhombus is a dark blue globe with twenty-seven small five-pointed white stars depicting a starry sky and a thin white convex horizontal band inscribed with the national motto 'Ordem e Progresso' across its center.",
+    },
   },
   {
     name: "Iceland",
     population: 334300,
     region: "Europe",
     capital: "Reykjavik",
+    flag: {
+      imageURL: "https://flagcdn.com/is.svg",
+      alt: "The flag of Iceland has a blue field with a large white-edged red cross that extends to the edges of the field. The vertical part of this cross is offset towards the hoist side.",
+    },
   },
   {
     name: "Afghanistan",
     population: 27657145,
     region: "Asia",
     capital: "Kabul",
+    flag: {
+      imageURL:
+        "https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_the_Taliban.svg",
+      alt: "The flag of the Islamic Emirate of Afghanistan has a white field with Arabic inscriptions — the Shahada — in black across its center.",
+    },
   },
   {
     name: "Aland Islands",
     population: 28875,
     region: "Europe",
     capital: "Mariehamn",
+    flag: {
+      imageURL: "https://flagcdn.com/ax.svg",
+      alt: "The flag of Aland Islands",
+    },
   },
   {
     name: "Albania",
     population: 2886026,
     region: "Europe",
     capital: "Tirana",
+    flag: {
+      imageURL: "https://flagcdn.com/al.svg",
+      alt: "The flag of Albania features a silhouetted double-headed black eagle at the center of a red field.",
+    },
   },
   {
     name: "Algeria",
     population: 404000000,
     region: "Africa",
     capital: "Algiers",
+    flag: {
+      imageURL: "https://flagcdn.com/dz.svg",
+      alt: "The flag of Algeria features two equal vertical bands of green and white. A five-pointed red star within a fly-side facing red crescent is centered over the two-color boundary.",
+    },
   },
 ];
