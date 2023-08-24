746d52e40afc6756ed156f080212c25eb431b7a2
Hector Figuereo <hectorfiguereo26@gmail.com>
2023-08-24 16:22:06 -0400

    Add dropdown custom hook

---
+++ src/components/Dropdown/useDropdown.ts
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { DropdownProps } from "./types";
+
+export const useDropdown = (props: DropdownProps) => {
+  const { selectedValue, options, className, onSelectOption } = props;
+
+  const [showOptions, setShowOptions] = useState(false);
+
+  const selectedOption = options.find((o) => o.value === selectedValue);
+
+  const handleClick = () => setShowOptions((prevState) => !prevState);
+
+  const handleKeyDown = (e: React.KeyboardEvent, cb: () => void) => {
+    if (e.code === "Space" || e.code === "Enter") cb();
+  };
+
+  return {
+    className,
+    showOptions,
+    options,
+    selectedOption,
+    onSelectOption,
+    setShowOptions,
+    handleClick,
+    handleKeyDown,
+  };
+};
