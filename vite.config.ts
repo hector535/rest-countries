4c71999b620f2857b4c991199757dd32e687f9b5
Hector Figuereo <hectorfiguereo26@gmail.com>
2023-08-26 12:48:13 -0400

    Update config file to incorporate testing configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  test: {
+    environment: "jsdom",
+    setupFiles: "./tests/setup.ts",
+  },
 });
